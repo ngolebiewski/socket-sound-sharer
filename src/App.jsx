28d11cbef810c5bc1d4261450e27c78d8b118b3b
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-07-22 12:15:14 -0400

    added in sound samples, set new Howls, and buttons to play them

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
+import { Howl } from 'howler'
+
+const dirtySample = '/sound/DirtySweptA1.wav'
+const stringSample = '/sound/MoogStringC2.wav'
+
+const sounds = {
+  dirtyMoog: new Howl({ src: [dirtySample], volume: 1 / 3 }),
+  stringMoog: new Howl({ src: [stringSample], volume: 1 / 3 }),
+};
 
 function App() {
-
+  const playSound = (sampleKey) => {
+    sounds[sampleKey].play();
+    console.log(`${sampleKey} played`)
+  }
   return (
     <>
       <h1>Socket Sound Scape Sharer</h1>
+      {Object.keys(sounds).map(sample => (
+        <button key={sample} onClick={() => playSound(sample)}>{sample}</button>
+      ))}
     </>
   )
 }
