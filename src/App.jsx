f715fea84bd0f599b56b48d973d902f53b34dd91
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-07-22 14:06:03 -0400

    add random stereo pan upon play sound

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { useEffect } from 'react'
 import { Howl } from 'howler'
 import { socket } from './socket';
 
-const dirtySample = '/sound/DirtySweptA1.wav'
-const stringSample = '/sound/MoogStringC2.wav'
+const loonSample = '/sound/Loon_robertcrosley.wav'
 
 const sounds = {
-  dirtyMoog: new Howl({ src: [dirtySample], volume: 1 / 3 }),
-  stringMoog: new Howl({ src: [stringSample], volume: 1 / 3 }),
+  dirtyMoog: new Howl({ src: ['/sound/DirtySweptA1.wav'], volume: 1 / 3 }),
+  stringMoog: new Howl({ src: ['/sound/MoogStringC2.wav'], volume: 1 / 3 }),
+  syncMoog: new Howl({ src: ['sound/Sync_HoldFsharp1.wav'], volume: 1 / 3}),
+  BassC1Moog: new Howl({ src: ['/sound/BassC1.wav'], volume: 1/3}),
+  BassC2Moog: new Howl({ src: ['/sound/BassC2.wav'], volume: 1/3}),
+  BassC3Moog: new Howl({ src: ['/sound/BassC3.wav'], volume: 1/3}),
+  loon: new Howl({ src: [loonSample], volume: 1 / 3 }),
+  fire: new Howl({ src: ['/sound/fire.wav'], volume: 1 / 3 }),
 };
+
+const randomStereoPan = () => {
+  const random = (Math.random() * 2 - 1).toFixed(1);
+  console.log('Random stereo pan -1 left, 1 right:', parseFloat(random))
+  return(parseFloat(random));
+}
 
 const App = () => {
 
   const playSound = (sampleKey) => {
+    sounds[sampleKey].stereo(randomStereoPan())
     sounds[sampleKey].play();
     socket.emit('playSound', sampleKey)
     console.log(`SENT: ${sampleKey} played and emitted to io Server`)
